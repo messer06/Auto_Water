0c34302e8b22041908467ce1ba7ca61738b95f8b
messer06 <ajmesser@gmail.com>
2019-02-23 19:01:17 -0600

    Update water.py

--- water.py
+++ water.py
@@ -29,7 +29,7 @@ def get_last_watered():
     except:
         return "NEVER!"
       
-def get_status(pin = 8,Moist_Hist):
+def get_status(Moist_Hist,pin = 8):
     GPIO.setup(pin, GPIO.IN,pull_up_down=GPIO.PUD_DOWN) 
     GPIO.setup(10,GPIO.OUT)
     GPIO.output(10,GPIO.HIGH)
