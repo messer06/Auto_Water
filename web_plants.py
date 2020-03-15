1116dea73edb88c6e80bfc44ffee5661cf46ac82
Messer06 <ajmesser@gmail.com>
2020-03-14 20:45:55 -0600

    Revert "Merge branch 'Moisture_History' into master"
    This reverts commit 735c3c47617c89fab90a27b479425a01da85aebf, reversing
    changes made to dee27510885537ef9e8860891819a8bb85f516ed.

--- web_plants.py
+++ web_plants.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, redirect, url_for,request
 import psutil
 import datetime
@@ -6,41 +5,32 @@ import water
 import os
 import cgi
 import subprocess
-import pandas as pd
+
 app = Flask(__name__)
 
-Moist_Hist = pd.DataFrame(columns=['DateTime','Status'])
-
 def template(title = "AutoWatering System", text = ""):
-    global Moist_Hist
     now = datetime.datetime.now()
     timeString = now
-    templateData = {
+    templateDate = {
         'title' : title,
         'time' : timeString,
-        'text' : text,
-        'Moist_Hist': Moist_Hist,
-        'tables': [Moist_Hist.to_html(classes='data')],
-        'titles': Moist_Hist.columns.values
+        'text' : text
         }
-    return templateData
+    return templateDate
 
 @app.route("/")
 def hello():
-    global Moist_Hist
     templateData = template()
     return render_template('main.html', **templateData)
 
 @app.route("/last_watered")
 def check_last_watered():
-    global Moist_Hist
     templateData = template(text = water.get_last_watered())
     return render_template('main.html', **templateData)
 
 @app.route("/sensor")
 def action():
-    global Moist_Hist
-    status = water.get_status(Moint)
+    status = water.get_status()
     message = ""
     if (status == 0):
         message = "Water me please!"
@@ -52,7 +42,6 @@ def action():
 
 @app.route("/water/<toggle>")
 def action2(toggle):
-    global Moist_Hist
     delay = int(toggle)
     water.pump_on(7,delay*60)
     templateData = template(text = "Watered Once")
@@ -60,7 +49,6 @@ def action2(toggle):
 
 @app.route("/auto/water/<toggle>")
 def auto_water(toggle):
-    global Moist_Hist
     running = False
     if toggle == "ON":
         templateData = template(text = "Auto Watering On")
